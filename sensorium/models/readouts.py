dd01b466a062a6de92b80286de4077f4d8b85cb0
Polina Turishcheva <polinaturishcheva@mb-polina.cidas.uni-goettingen.de>
2023-03-29 16:27:37 +0200

    initial commit

---
+++ sensorium/models/readouts.py
@@ -0,0 +1,10 @@
+from neuralpredictors.layers.readouts import MultiReadoutSharedParametersBase, FullGaussian2d, FullFactorized2d
+
+
+class MultipleFullGaussian2d(MultiReadoutSharedParametersBase):
+    _base_readout = FullGaussian2d
+    
+
+class MultipleFullFactorized2d(MultiReadoutSharedParametersBase):
+    _base_readout = FullFactorized2d
+    
